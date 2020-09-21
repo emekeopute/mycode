dd05102736a1943db8bc765fa3b0b53a84ee56ca
Emeke Opute <emeke.opute@outlook.com>
2020-09-21 19:24:25 +0000

    new scripts

---
+++ mixlist/mylistprint.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# create a list containing several items
+challenge_list = ["awesome", "terrible", "supoib", "Python", "banana", 1, 2]
+
+# prints out using f-string 
+print(f"{challenge_list[3]} is {challenge_list[0]}! For lunch I will have {challenge_list[6]} {challenge_list[4]}s.")
