62534497efe04d7f28e727e73ae09db81c15663e
Emeke Opute <emeke.opute@outlook.com>
2020-09-23 22:01:14 +0000

    Multiple codes

---
+++ loopchallenge/grandmathreats.py
@@ -0,0 +1,6 @@
+# For every [dirty dish] I find in your [room], that's how many times I'm going to [whoop your ass].
+
+room = ["bowl", "plate", "fork"]
+
+for dish in room:
+    print(f"There's a dirty {dish} in your room, WHOOP!")
---
+++ loopchallenge/grandmathreats2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+count = 0
+hallway = ["toy", "frame", "toy"]
+
+for toy in hallway:
+    if toy == "toy"
+        count =+ 1
+        hallway.remove(count)
+        print(f"I found a {toy} in the hallway and removed it")
+        print(hallway)
