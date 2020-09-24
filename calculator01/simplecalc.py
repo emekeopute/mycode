4a5b7ce4453c0d4e3ab10be851be30fe2355a902
Emeke Opute <emeke.opute@outlook.com>
2020-09-24 15:50:32 +0000

    Simple Calculator

---
+++ calculator01/simplecalc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Amazon || Author: Emeke Opute"""
+
+user_operation = input("Please enter 1 for add, enter 2 for subtract, 3 for divide, 4 for multiply: ")
+
+if int(user_operation) == 1
+    my_add_list = []
+    num = input("Please enter the first number you want to add: ")
+        try:
+            float(num1)
+        except ValueError:
+            print "Not a valid number, please enter an integer or a decimal"
+        my_add_list.append(num)
+
+    """    
+    num2 = input("Please enter the first number you want to add: ")
+        try:
+            float(num1)
+        except ValueError:
+            print "Not a valid number, please enter an integer or a decimal"
+    """
+    Sum = sum(my_add_list)
+    print(f"Sum: {Sum}")
+    
+
